b86abec427a5bdd14e54c04fbf38beed722e77e2
PaulBoersma <paul.boersma@uva.nl>
2025-12-31 18:33:51 +0100

    removed LPCFrameIntoFormantFrame

--- LPC/Sound_to_Formant_mt.h
+++ LPC/Sound_to_Formant_mt.h
@@ -24,29 +24,6 @@
 #include "LPC_and_Formant.h"
 #include "SampledFrameIntoSampledFrame.h"
 
-#if 0
-Thing_define (SoundFrameIntoFormantFrame, SampledFrameIntoSampledFrame) {
-
-	autoSoundFrameIntoLPCFrame soundIntoLPC;
-	autoLPCFrameIntoFormantFrame lpcIntoFormant;
-
-	void initBasicSoundFrameIntoFormantFrame (constSound inputSound, mutableFormant outputFormant);
-	
-	void initHeap ()
-		override;
-		
-	void getInputFrame (integer iframe) override {
-		soundIntoLPC -> getInputFrame (iframe);
-	}
-	
-	bool inputFrameIntoOutputFrame (integer iframe)
-		override;
-};
-
-autoSoundFrameIntoFormantFrame SoundFrameIntoFormantFrame_create (SoundFrameIntoLPCFrame soundIntoLPC, 
-	LPCFrameIntoFormantFrame lpcIntoFormant);
-#endif
-
 void Sound_into_Formant_robust_mt (constSound me, Formant out,	double windowLength,
 	integer numberOfPoles, double safetyMargin, double k, integer itermax, double tol, double location, bool wantlocation
 );
